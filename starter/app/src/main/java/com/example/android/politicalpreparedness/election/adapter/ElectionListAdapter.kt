88ef87d82d4d984413a1485c547317c7e2808374
wolsz <wolsz1@gmail.com>
2021-07-22 00:56:52 -0400

    First Adapters

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -1,23 +1,61 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.example.android.politicalpreparedness.databinding.ViewholderElectionBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ElectionItemBinding
 import com.example.android.politicalpreparedness.network.models.Election
 
-class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
+class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionListAdapter.ElectionViewHolder>(ElectionDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
         return ElectionViewHolder.from(parent)
     }
 
     //TODO: Bind ViewHolder
+    override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
+        val item = getItem(position)
+
+        holder.bind(clickListener,item)
+    }
 
     //TODO: Add companion object to inflate ViewHolder (from)
+    class ElectionViewHolder private constructor(val binding: ElectionItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(clickListener: ElectionListener, item: Election) {
+            binding.election = item
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ElectionViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ElectionItemBinding.inflate(layoutInflater, parent, false)
+
+                return ElectionViewHolder(binding)
+            }
+        }
+    }
 }
 
 //TODO: Create ElectionViewHolder
 
 //TODO: Create ElectionDiffCallback
+class ElectionDiffCallback : DiffUtil.ItemCallback<Election>() {
+    override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Election, newItem: Election): Boolean {
+        return oldItem == newItem
+    }
+}
 
 //TODO: Create ElectionListener
+class ElectionListener(val clickListener: (electionId: Int) -> Unit) {
+    fun onClick(election: Election) = clickListener(election.id)
+}
--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -1,12 +1,18 @@
 package com.example.android.politicalpreparedness.network
 
 import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
+import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 import java.util.*
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
@@ -31,10 +37,19 @@ private val retrofit = Retrofit.Builder()
 
 interface CivicsApiService {
     //TODO: Add elections API Call
+    @GET("elections")
+    fun getElectionsResults(): Deferred<ElectionResponse>
 
     //TODO: Add voterinfo API Call
+    @GET("voterinfo")
+    fun getVoterInfoResults(
+            @Query("electionId") electionId: Int,
+            @Query("address") address: String): Deferred<VoterInfoResponse>
 
     //TODO: Add representatives API Call
+    @GET("representatives")
+    fun getRepresentativesResults(
+            @Query("address") address: String): Deferred<RepresentativeResponse>
 }
 
 object CivicsApi {
