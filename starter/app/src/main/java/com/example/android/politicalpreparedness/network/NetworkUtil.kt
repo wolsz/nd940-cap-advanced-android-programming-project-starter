fba000a3d54642c75f1708ec3a10b157780507d6
wolsz <wolsz1@gmail.com>
2021-10-13 21:55:26 -0400

    More working on RepresentativeFragment.kt part 2

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/NetworkUtil.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/NetworkUtil.kt
@@ -3,6 +3,7 @@ package com.example.android.politicalpreparedness.network
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.os.Build
 import android.util.Log
 
 class NetworkUtil {
@@ -10,20 +11,55 @@ class NetworkUtil {
         fun isOnline(context: Context): Boolean {
             val connectivityManager =
                     context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            if (connectivityManager != null) {
-                val capabilities =
-                        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-                if (capabilities != null) {
-                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+            val capabilities =
+                    connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
                         Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
                         return true
-                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
                         Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
                         return true
-                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
                         Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
                         return true
                     }
+                }
+            }
+            return false
+        }
+
+
+        fun isConnected(context: Context): Boolean {
+            val connectivityManager =
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val capabilities =
+                        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                capabilities?.let {
+                    when {
+                        it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                            Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                            return true
+                        }
+                        it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                            Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                            return true
+                        }
+                        it.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                            Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                            return true
+                        }
+                        else -> false
+                    }
+                }
+            } else {
+                @Suppress("DEPRECATION") val activeNetworkInfo = connectivityManager.activeNetworkInfo
+                @Suppress("DEPRECATION")if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                    return true
                 }
             }
             return false
--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.android.politicalpreparedness.representative
 
 import android.Manifest
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
@@ -9,13 +11,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentRepresentativeBinding
 import com.example.android.politicalpreparedness.network.models.Address
 import com.example.android.politicalpreparedness.representative.adapter.RepresentativeListAdapter
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+//import com.google.android.gms.location.R
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 class RepresentativeFragment : Fragment() {
@@ -23,12 +33,16 @@ class RepresentativeFragment : Fragment() {
     companion object {
         //TODO: Add Constant for Location request
         private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100
+        const val REQUEST_TURN_DEVICE_LOCATION_ON = 5
     }
+
     private lateinit var representativeAdapter: RepresentativeListAdapter
 
     private val viewModel: RepresentativeViewModel by lazy {
         ViewModelProvider(this).get(RepresentativeViewModel::class.java)
     }
+
+    private val TAG = RepresentativeFragment::class.java.simpleName
 
     //TODO: Declare ViewModel
 
@@ -49,45 +63,62 @@ class RepresentativeFragment : Fragment() {
 
         //TODO: Establish button listeners for field and location search
 
+        checkDeviceLocationSettings()
         return binding.root
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //TODO: Handle location permission result to get location on permission granted
+
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+   //             checkLocationPermissions()
+                Toast.makeText(requireContext(), "Permission granted in Request Permission, moving on....", Toast.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(
+                        requireView(),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_LONG
+                ).show()
+            }
+
+        }
     }
 
     private fun checkLocationPermissions(permissionString: String, requestCode: Int) {
         when {
             ContextCompat.checkSelfPermission(requireContext(),
                     permissionString) == PackageManager.PERMISSION_GRANTED -> {
-                Log.d("CHECKPERMISSIONS", "checkLocationPermissions: Permission granted")
-                    }
-            shouldShowRequestPermissionRationale(permissionString) -> showReasonDialog(permissionString,  requestCode)
-            else -> requestPermissions(arrayOf(permissionString), requestCode)
+                Log.d("checkLocationPermissions", "checkLocationPermissions: Permission granted")
+                Toast.makeText(requireContext(), "Permission granted in Check Location Permission", Toast.LENGTH_LONG).show()
+            }
+            shouldShowRequestPermissionRationale(permissionString) -> showReasonDialog(permissionString, requestCode)
+            else -> {
+                Log.d("checkLocationPermissions", "checkLocationPermissions: Requesting permission in ")
+                requestPermissions(arrayOf(permissionString), requestCode)}
         }
     }
 
     private fun showReasonDialog(permissionString: String, requestCode: Int) {
-        val alertbuilder = AlertDialog.Builder(requireContext())
+        val alertBuilder = AlertDialog.Builder(requireContext())
+
+        alertBuilder.apply {
+            setMessage(R.string.permission_denied_explanation)
+            setTitle("Permission required")
+            setPositiveButton("OK") { dialog, which ->
+                requestPermissions(arrayOf(permissionString), requestCode)
+            }
+        }
+        val dialog = alertBuilder.create()
+        dialog.show()
     }
 
 
-//    private fun checkLocationPermissions(): Boolean {
-//        return if (isPermissionGranted()) {
-//            true
-//        } else {
-//            //TODO: Request Location permissions
-//            false
-//        }
+//    private fun isPermissionGranted(): Boolean {
+//        return ContextCompat.checkSelfPermission(requireContext(),
+//                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 //    }
-
-
-
-    private fun isPermissionGranted() : Boolean {
-        return ContextCompat.checkSelfPermission(requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
 
     private fun getLocation() {
         //TODO: Get location from LocationServices
@@ -103,9 +134,51 @@ class RepresentativeFragment : Fragment() {
                 .first()
     }
 
-//    private fun hideKeyboard() {
-//        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view!!.windowToken, 0)
-//    }
+
+    private fun checkDeviceLocationSettings(resolve: Boolean = true) {
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_LOW_POWER
+        }
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val settingsClient = LocationServices.getSettingsClient(requireContext())
+        val locationSettingsResponseTask = settingsClient.checkLocationSettings(builder.build())
+        locationSettingsResponseTask.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException && resolve) {
+                try {
+                    this.startIntentSenderForResult(
+                            exception.resolution.intentSender, REQUEST_TURN_DEVICE_LOCATION_ON,
+                            null, 0, 0, 0, null
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    Log.d(
+                            TAG,
+                            "checkDeviceLocationSettings: Error getting location settings resolution: " + sendEx.message
+                    )
+                }
+            } else {
+                Log.i(TAG, "checkDeviceLocationSettings: Snackbar")
+                Snackbar.make(
+                        requireView(),
+                        R.string.location_required_error, Snackbar.LENGTH_INDEFINITE
+                ).setAction(android.R.string.ok) {
+                    checkDeviceLocationSettings()
+                }.show()
+            }
+        }
+        locationSettingsResponseTask.addOnCompleteListener {
+            if (it.isSuccessful) {
+                checkLocationPermissions(Manifest.permission.ACCESS_FINE_LOCATION, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+                Log.i(TAG, "checkDeviceLocationSettingsAndSave: Device location setting is on")
+
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_TURN_DEVICE_LOCATION_ON) {
+            checkDeviceLocationSettings(false)
+        }
+    }
 
 }
